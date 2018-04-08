7140df80e9ac43acebe3df9c30278670359d5732
Tom Deadline <lostinsidemybed@gmail.com>
2018-04-08 23:50:22 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,36 @@
+[{
+                    ProductName: 'Harry',
+                    UPC: '1232131231',
+                    Pageviews: [3, 2, 54, 5, 3, 2, 4, 32, ],
+                    img: 'http://ca-media.contentanalyticsinc.com/ca-test/mc/186756/1/4fff3b8ba5d643167086e26a972023ad.jpeg'
+                },
+                {
+                    ProductName: 'Zane',
+                    UPC: '4324423413',
+                    Pageviews: [3, 4, 5, 62, 3, 6, 3, 3],
+                    img: 'http://ca-media.contentanalyticsinc.com/ca-test/mc/186756/1/4fff3b8ba5d643167086e26a972023ad.jpeg'
+                },
+                {
+                    ProductName: 'Delux',
+                    UPC: '43423422413',
+                    Pageviews: [1, 3, 4, 6, 2, 4, 6, 2],
+                    img: 'http://ca-media.contentanalyticsinc.com/ca-test/mc/186756/1/4fff3b8ba5d643167086e26a972023ad.jpeg'
+                },
+                {
+                    ProductName: 'papa',
+                    UPC: '4354313',
+                    Pageviews: [4, 2, 5, 12, 32, 1, 23, 4],
+                    img: 'http://ca-media.contentanalyticsinc.com/ca-test/mc/186756/1/4fff3b8ba5d643167086e26a972023ad.jpeg'
+                },
+                {
+                    ProductName: 'juice',
+                    UPC: '12352414',
+                    Pageviews: [42, 4, 15, 2, 2, 43, 3, 4],
+                    img: 'http://ca-media.contentanalyticsinc.com/ca-test/mc/186756/1/4fff3b8ba5d643167086e26a972023ad.jpeg'
+                },
+                {
+                    ProductName: 'cola',
+                    UPC: '42341432',
+                    Pageviews: [41, 8, 13, 24, 2, 11, 43, 4],
+                    img: 'http://ca-media.contentanalyticsinc.com/ca-test/mc/186756/1/4fff3b8ba5d643167086e26a972023ad.jpeg'
+                }]
